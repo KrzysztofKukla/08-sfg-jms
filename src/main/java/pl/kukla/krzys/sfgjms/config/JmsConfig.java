f6dc48cfab905bd2c7dfa0277ebfc31717aea264
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-02-20 22:13:21 +0100

    176 MesageConverter configuration

---
+++ src/main/java/pl/kukla/krzys/sfgjms/config/JmsConfig.java
@@ -0,0 +1,27 @@
+package pl.kukla.krzys.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author Krzysztof Kukla
+ */
+@Configuration
+public class JmsConfig {
+
+    //when we send the message to JMS, Spring converts that message to JMS text message
+    //and the payload takes Java object and converts to JSON payload
+    //conversion between JMS message and from Java Object into serialized JSON into JMS message
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+
+}
