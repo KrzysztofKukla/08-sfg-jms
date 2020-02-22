0847a4faacf94700488cc5b8797c0f9b98926c09
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-02-22 18:52:56 +0100

    181 configure ActiveMQ Docker in Spring Boot and using it

--- src/main/java/pl/kukla/krzys/sfgjms/Application.java
+++ src/main/java/pl/kukla/krzys/sfgjms/Application.java
@@ -1,8 +1,5 @@
 package pl.kukla.krzys.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +8,14 @@ public class Application {
 
 	public static void main(String[] args) throws Exception {
 
-		ActiveMQServer mqServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-			.setPersistenceEnabled(false)
-			.setJournalDirectory("target/data/journal")
-			.setSecurityEnabled(false)
-			.addAcceptorConfiguration("invm", "vm://0")
-		);
-
-		mqServer.start();
+		//for embedded server
+//		ActiveMQServer mqServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//			.setPersistenceEnabled(false)
+//			.setJournalDirectory("target/data/journal")
+//			.setSecurityEnabled(false)
+//			.addAcceptorConfiguration("invm", "vm://0")
+//		);
+//		mqServer.start();
 
 		SpringApplication.run(Application.class, args);
 	}
